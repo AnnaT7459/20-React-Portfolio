dac00f1ea00369438cf1f5df1a51c8a61dfc92a2
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-14 14:28:37 -0600

    logic added and bio written for About Me section

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function AboutMe() {
+// structure for About Me section
+  return (
+    <section className="about-me">
+      <h2>About Me</h2>
+      <div className="bio">
+        <img src="avatar.jpg" alt="Avatar" />
+        <p>Hello there! My name is Anna Turner and I am currently enrolled as a coding bootcamp student with EdX through the University of Kansas. I am in the midst of a career change going from a teacher to web developer. I currently reside in Kansas City, Missouri with my two cats, Tundra and Crookshanks. In my spare time I love to read and visit local micro breweries. My skills include: HTML, CSS, JavaScript, Web APIs, Third Party APIs, Server Side APIs, NodeJS, Object-Relational Mapping, Model-Viewer-Controller Express,SQL, NoSQL, Progressive Web Applications, React, MERN, State, Markdown, Git, and Github Repo Managment.</p>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
