9483ef093dd65563b8ec3684141f3ea238720a15
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-14 11:51:50 -0600

    added file structure and set up app react logic

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  const [currentSection, setCurrentSection] = useState('About Me');
+  
+  const handleNavigationClick = (section) => {
+    setCurrentSection(section);
+  };
+
+  return (
+    <div className="App">
+      <Header currentSection={currentSection} handleNavigationClick={handleNavigationClick} />
+      <main>
+        {currentSection === 'About Me' && <AboutMe />}
+        {currentSection === 'Portfolio' && <Portfolio />}
+        {currentSection === 'Contact' && <Contact />}
+        {currentSection === 'Resume' && <Resume />}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
