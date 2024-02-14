47492a1e5f25415eeef077501dbd2fa5b1767142
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-14 12:11:32 -0600

    added comments to describe the functionality of the app and logic/comments for the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// different sections of portfolio
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
@@ -8,12 +9,14 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  // defaults to About Me when initialized
   const [currentSection, setCurrentSection] = useState('About Me');
   
+  // updates the current section when nav links are clicked
   const handleNavigationClick = (section) => {
     setCurrentSection(section);
   };
-
+  // conditional statements based off of what link is clicked. These sections will render based off of those clicks, but the header and footer will remain on each.
   return (
     <div className="App">
       <Header currentSection={currentSection} handleNavigationClick={handleNavigationClick} />
@@ -27,5 +30,5 @@ function App() {
     </div>
   );
 }
-
+// can be used in other parts of the application-
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+// 2 props for header section of the webpage
+function Header({ currentSection, handleNavigationClick }) {
+    // structure of header
+  return (
+    <header className="header">
+      <h1>Anna Turner</h1>
+      <h2>Professional Portfolio</h2>
+      {/* navigation bar */}
+      <nav>
+        <ul>
+            {/* if the corresponding section matches the current section prop, then it will be active, handler function is called with specific section */}
+          <li className={currentSection === 'About Me' ? 'active' : ''} onClick={() => handleNavigationClick('About Me')}>About Me</li>
+          <li className={currentSection === 'Portfolio' ? 'active' : ''} onClick={() => handleNavigationClick('Portfolio')}>Portfolio</li>
+          <li className={currentSection === 'Contact' ? 'active' : ''} onClick={() => handleNavigationClick('Contact')}>Contact</li>
+          <li className={currentSection === 'Resume' ? 'active' : ''} onClick={() => handleNavigationClick('Resume')}>Resume</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
