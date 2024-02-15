bd9df0b07a6e530cf8f2593e257d56e862a9b823
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-14 18:09:28 -0600

    added third platform to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ function Footer() {
       <ul>
         <li><a href="https://github.com/AnnaT7459">GitHub</a></li>
         <li><a href="https://www.linkedin.com/in/anna-turner-09839a228/">LinkedIn</a></li>
+        <li><a href="https://stackoverflow.com/users/22329046/anna-turner">Stack Overflow</a></li>
       </ul>
     </footer>
   );
