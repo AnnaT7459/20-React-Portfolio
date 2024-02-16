4f0c5756adf3aad87069fc1a84e005c167ebd7a5
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-15 19:14:33 -0600

    added mobile-first design to css, need to work on sizing portfolio image sizes

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
     <section className="about-me">
       <h2>About Me</h2>
       <div className="bio">
-        <img src="avatar.jpg" alt="Avatar" />
+        {/* <img src= pic of me /> */}
         <p>Hello there! My name is Anna Turner and I am currently enrolled as a coding bootcamp student with EdX through the University of Kansas. I am in the midst of a career change going from a teacher to web developer. I currently reside in Kansas City, Missouri with my two cats, Tundra and Crookshanks. In my spare time I love to read and visit local micro breweries. My skills include: HTML, CSS, JavaScript, Web APIs, Third Party APIs, Server Side APIs, NodeJS, Object-Relational Mapping, Model-Viewer-Controller Express,SQL, NoSQL, Progressive Web Applications, React, MERN, State, Markdown, Git, and Github Repo Managment.</p>
       </div>
     </section>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,47 +2,46 @@ import * as images from '../assets/images/index'
 
 const projects = [
     {
-        title: 'ClockWise',
-        imageUrl: '../assets/images/clockwise.png',
-        link: 'https://nameless-fjord-16797-0078e08b982c.herokuapp.com/login',
+      title: "ClockWise",
+      imageName: "clockwise",
+      link: "https://nameless-fjord-16797-0078e08b982c.herokuapp.com/login",
     },
     {
-        title: 'Note Taker',
-        imageUrl: '../assets/images/note-taker.png',
-        link: 'https://sleepy-anchorage-41730-09a2e414e230.herokuapp.com/',
+      title: "Note Taker",
+      imageName: "noteTaker",
+      link: "https://sleepy-anchorage-41730-09a2e414e230.herokuapp.com/",
     },
     {
-        title: 'RecipeNutri Group Project',
-        imageUrl: '../assets/images/recipenutri.png',
-        link: 'https://natemcmahon.github.io/Recipe-Nutri/',
+      title: "RecipeNutri Group Project",
+      imageName: "recipeNutri",
+      link: "https://natemcmahon.github.io/Recipe-Nutri/",
     },
     {
-        title: 'Weather Dashboard',
-        imageUrl: '../assets/images/weather-dashboard.png',
-        link: 'https://annat7459.github.io/6-weather-dashboard/',
+      title: "Weather Dashboard",
+      imageName: "weatherDash",
+      link: "https://annat7459.github.io/6-weather-dashboard/",
     },
     {
-        title: 'Work Day Scheduler',
-        imageUrl: '../assets/images/work-day-scheduler.png',
-        link: 'https://annat7459.github.io/5-work-day-scheduler/',
+      title: "Work Day Scheduler",
+      imageName: "workSched",
+      link: "https://annat7459.github.io/5-work-day-scheduler/",
     },
     {
-        title: 'Coding Quiz',
-        imageUrl: '../assets/images/coding-quiz.png',
-        link: 'https://annat7459.github.io/4-code-quiz/',
+      title: "Coding Quiz",
+      imageName: "codingQuiz",
+      link: "https://annat7459.github.io/4-code-quiz/",
     },
     {
-        title: 'Password Generator',
-        imageUrl: '../password-generator.png',
-        link: 'https://annat7459.github.io/3-password-generator/',
+      title: "Password Generator",
+      imageName: "passwordGen",
+      link: "https://annat7459.github.io/3-password-generator/",
     },
     {
-        title: 'Code Refactor',
-        imageUrl: '../assets/images/clockwise.png',
-        link: 'https://annat7459.github.io/code-refactor/',
+      title: "Code Refactor",
+      imageName: "codeRefactor",
+      link: "https://annat7459.github.io/code-refactor/",
     },
-
-];
+  ];
 
 export default function Portfolio() {
     // renders the portfolio page
@@ -55,7 +54,7 @@ export default function Portfolio() {
                     // links the project to the URL. key prop is set to index for an id of each project, also allows for styling to be done with the class name
                     <a key={index} href={project.link} className="project-link">
                         {/* the background image will be the screenshot and the title should be nested on the screenshot */}
-                        <img className="project" src = {images [index]}>
+                        <img className="project" src={images[project.imageName]}>
                         </img>
                         <div className="project-title">{project.title}</div>
                     </a>
