6fdc839e5828e4fcc123085073b9316099dc6307
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-19 11:28:04 -0600

    added photo of me to About Me section

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
     <section className="about-me">
       <h2>About Me</h2>
       <div className="bio">
-        {/* <img src= pic of me /> */}
+        <img src='src\assets\images\IMG_7761.JPG'></img>
         <p>Hello there! My name is Anna Turner and I am currently enrolled as a coding bootcamp student with EdX through the University of Kansas. I am in the midst of a career change going from a teacher to web developer. I currently reside in Kansas City, Missouri with my two cats, Tundra and Crookshanks. In my spare time I love to read and visit local micro breweries.  </p>
       </div>
     </section>
