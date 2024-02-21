4bc3dca2cbea2cb1e6813e33020c7844cacedfbb
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-02-20 18:38:05 -0600

    made repo clickable text instead of long web address

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -29,7 +29,7 @@ const projects = [
     title: "Work Day Scheduler",
     imageName: "workSched",
     link: "https://annat7459.github.io/5-work-day-scheduler/",
-    repo:"https://github.com/AnnaT7459/5-work-day-scheduler",
+    repo: "https://github.com/AnnaT7459/5-work-day-scheduler",
   },
   {
     title: "Coding Quiz",
@@ -52,23 +52,23 @@ const projects = [
 ];
 
 export default function Portfolio() {
-    // renders the portfolio page
-    return (
-        <section className="portfolio">
-            <h2>Portfolio</h2>
-            <div className="projects">
-                {/* iterates over the projects array above to create indiviudal elements */}
-                {projects.map((project, index) => (
-                    // links the project to the URL. key prop is set to index for an id of each project, also allows for styling to be done with the class name
-                    <a key={index} href={project.link} className="project-link">
-                        <img className="project" src={images[project.imageName]}>
-                        </img>
+  // renders the portfolio page
+  return (
+    <section className="portfolio">
+      <h2>Portfolio</h2>
+      <div className="projects">
+        {/* iterates over the projects array above to create indiviudal elements */}
+        {projects.map((project, index) => (
+          // links the project to the URL. key prop is set to index for an id of each project, also allows for styling to be done with the class name
+          <a key={index} href={project.link} className="project-link">
+                        <img className="project" src={images[project.imageName]} alt={project.title} />
                         <div className="project-title">{project.title}</div>
-                        <div className="project-repo">{project.repo}</div>
+                        {/* Displaying the repository link as clickable text */}
+                        <a href={project.repo} className="project-repo">Project Repository</a>
                     </a>
-                ))}
-            </div>
-        </section>
-    );
+        ))}
+      </div>
+    </section>
+  );
 }
 
